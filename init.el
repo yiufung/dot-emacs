6d39ae32a054ecadf399aa0b56d25f4e482e09ce
Cheong Yiu Fung <cheongyiufung@gmail.com>
2020-01-13 10:00:42 +0800

    update for anki-editor

--- init.el
+++ init.el
@@ -198,7 +198,8 @@
 ;; Set paste error under linux
 (set-selection-coding-system 'utf-8)
 ;; Allow pasting selection outside of Emacs
-(setq x-select-enable-clipboard t)
+(setq x-select-enable-clipboard t
+      x-select-enable-primary t)
 ;; Don't blink
 (blink-cursor-mode 0)
 
@@ -1318,12 +1319,12 @@ horizontal mode."
           ("a" "Anki basic"
            entry
            (file+headline org-my-anki-file "Dispatch Shelf")
-           "* %<%H:%m>   %^g\n:PROPERTIES:\n:ANKI_NOTE_TYPE: Basic\n:ANKI_DECK: Mega\n:END:\n** Front\n%?\n** Back\n%x\n")
+           "* %<%H:%M>   %^g\n:PROPERTIES:\n:ANKI_NOTE_TYPE: Basic\n:ANKI_DECK: Mega\n:END:\n** Front\n%?\n** Back\n%x\n")
 
           ("A" "Anki cloze"
            entry
            (file+headline org-my-anki-file "Dispatch Shelf")
-           "* %^{Card Name}\n:PROPERTIES:\n:ANKI_NOTE_TYPE: Cloze:ANKI_DECK: Mega\n\n:END:\n** Text\n%^{Front}\n** Extra\n%?\n")
+           "* %<%H:%M>   %^g\n:PROPERTIES:\n:ANKI_NOTE_TYPE: Cloze\n:ANKI_DECK: Mega\n:END:\n** Text\n%x\n** Extra\n")
 
           ("c" "all todos" ;; Capture first, refile later
            entry
@@ -3612,9 +3613,39 @@ In that case, insert the number."
 
 (use-package anki-editor
   :defer 10
+  :after org
+  :bind (:map org-mode-map
+              ("<f12>" . anki-editor-cloze-region-auto-incr)
+              ("<f11>" . anki-editor-cloze-region-dont-incr)
+              ("<f10>" . anki-editor-reset-cloze-number)
+              ("<f9>"  . anki-editor-push-tree))
   :config
   (setq anki-editor-create-decks t
-        anki-editor-org-tags-as-anki-tags t))
+        anki-editor-org-tags-as-anki-tags t)
+
+  (defun anki-editor-cloze-region-auto-incr (&optional arg)
+    "Cloze region without hint and increase card number."
+    (interactive)
+    (anki-editor-cloze-region my-anki-editor-cloze-number "")
+    (setq my-anki-editor-cloze-number (1+ my-anki-editor-cloze-number))
+    (forward-sexp))
+  (defun anki-editor-cloze-region-dont-incr (&optional arg)
+    "Cloze region without hint using the previous card number."
+    (interactive)
+    (anki-editor-cloze-region (1- my-anki-editor-cloze-number) "")
+    (forward-sexp))
+  (defun anki-editor-reset-cloze-number (&optional arg)
+    "Reset cloze number to ARG or 1"
+    (interactive)
+    (setq my-anki-editor-cloze-number (or arg 1)))
+  (defun anki-editor-push-tree ()
+    "Push all notes under a tree."
+    (interactive)
+    (anki-editor-push-notes '(4))
+    (anki-editor-reset-cloze-number))
+  ;; Initialize
+  (anki-editor-reset-cloze-number)
+  )
 
 (use-package howdoyou
   :defer 10)
