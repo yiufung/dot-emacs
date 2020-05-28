2129dca588bde53eb1e850583bcbde15bffdd499
Cheong Yiu Fung <cheongyiufung@gmail.com>
2020-05-28 09:51:33 +0800

    misc changes

--- init.el
+++ init.el
@@ -1171,6 +1171,15 @@ horizontal mode."
   (setq eyebrowse-keymap-prefix (kbd "C-c w")) ;; w for workspace
   :bind
   (
+   ("M-1" . 'eyebrowse-switch-to-window-config-1)
+   ("M-2" . 'eyebrowse-switch-to-window-config-2)
+   ("M-3" . 'eyebrowse-switch-to-window-config-3)
+   ("M-4" . 'eyebrowse-switch-to-window-config-4)
+   ("M-5" . 'eyebrowse-switch-to-window-config-5)
+   ("M-6" . 'eyebrowse-switch-to-window-config-6)
+   ("M-7" . 'eyebrowse-switch-to-window-config-7)
+   ("M-8" . 'eyebrowse-switch-to-window-config-8)
+   ("M-9" . 'eyebrowse-switch-to-window-config-9)
    ("C-c w s"   . 'eyebrowse-switch-to-window-config)
    ("C-c w k"   . 'eyebrowse-close-window-config)
    ("C-c w w"   . 'eyebrowse-switch-to-window-config)
@@ -1184,7 +1193,7 @@ horizontal mode."
   (setq eyebrowse-wrap-around t
         eyebrowse-close-window-config-prompt t
         eyebrowse-mode-line-style 'smart
-        eyebrowse-tagged-slot-format "%t"
+        eyebrowse-tagged-slot-format "%s:%t"
         eyebrowse-new-workspace nil)
   (eyebrowse-mode)
   )
@@ -4025,8 +4034,8 @@ In that case, insert the number."
       (progn
         (message "[cyf] Light theme detected: Setting backgrounds to floral white")
         (set-background-color "floral white")
-        )
-    ))
+        ))
+  )
 
 (defun cyf/set-dark-theme-highlight-region ()
   "In dark themes it's difficult to see where it highlights the
